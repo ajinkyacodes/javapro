594e94ad9bdb1cab4e992ab668148c7a72f18a30
AJINKYA <ajinkyacodes@gmail.com>
2025-08-01 01:54:42 +0530

    Exercise1 solved

---
+++ src/Core_Java/Exercises/Exercise1.java
@@ -0,0 +1,14 @@
+package Core_Java.Exercises;
+
+/*
+Exercise 1: Variables & data types Coding Exercise
+ */
+public class Exercise1 {
+    public static void main(String[] args) {
+        // write code here
+        int age = 25;
+        double height = 5.9;
+        String name = "Alex";
+        System.out.println("Name: " + name + ", Age: " + age + ", Height: " + height);
+    }
+}
