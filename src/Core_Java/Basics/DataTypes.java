296bd581d0b5b0314d656d8562ffb5ff3d03fb90
AJINKYA <ajinkyacodes@gmail.com>
2025-08-01 01:49:04 +0530

    DataTypes code added

---
+++ src/Core_Java/Basics/DataTypes.java
@@ -0,0 +1,40 @@
+package Core_Java.Basics;
+
+/*
+Data Types: Primitive & Non-Primitive
+
+# Primitive Data Types
+Integer -> byte, short, int, long
+Float -> double, float
+Character -> char
+Boolean -> boolean
+
+# Integer
+int - 4 bytes (-2^31 to 2^31 - 1)
+long - 8 bytes (-2^63 to 2^63 - 1)
+short - 2 bytes (-2^15 to 2^15 - 1)
+byte - 1 byte (-2^7 to 2^7 - 1)
+
+float - 4 bytes
+double - 8 bytes
+
+char - 2 bytes UNICODE e.g. char c = 'k'
+
+boolean - true or false
+ */
+
+public class DataTypes {
+    public static void main(String[] args) {
+        int a = 123;
+        byte num = 127;
+        short sh = 558;
+        long l = 54121221L;
+
+        float price = 856.5f;
+        double cgpa = 7.1;
+
+        char letter = 'k';
+
+        boolean isAvailable = true;
+    }
+}
