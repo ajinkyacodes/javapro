c9842d11791537ce59effebdd5ad2ea8d3d79860
AJINKYA <ajinkyacodes@gmail.com>
2025-08-01 01:34:05 +0530

    Variables code added

---
+++ src/Core_Java/Basics/Variables.java
@@ -0,0 +1,10 @@
+package Core_Java.Basics;
+
+public class Variables {
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 5;
+        int result = num1 + num2;
+        System.out.println(result);
+    }
+}
